a0f9a91c86c2075e18c48aa00ede652cc20a6d33
Kylymbek <kkyly.2000@gmail.com>
2022-01-27 16:34:16 +0600

    student screen

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import {StyleSheet} from 'react-native';
 import WelcomeStack from './stacks/WelcomeStack';
 import ParentStack from './stacks/ParentStack';
+import StudentStack from './stacks/StudentStack';
 
 const App = () => {
   const is_authorized = true;
+  const user_type = 'student';
 
   return (
     <>
       {!is_authorized ? (
         <WelcomeStack />
-      ) : (
-        <>
-          <ParentStack />
-        </>
-      )}
+      ) : user_type === 'parent' ? (
+        <ParentStack />
+      ) : user_type === 'student' ? (
+        <StudentStack />
+      ) : user_type === 'seller' ? (
+        <ParentStack />
+      ) : null}
     </>
   );
 };
---
+++ src/stacks/StudentStack.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function StudentStack() {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+export default StudentStack;
