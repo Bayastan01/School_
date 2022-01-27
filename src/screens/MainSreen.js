847e1c8b9289f7ed0f1ccb6b1e3ba18ddd387495
Bayel <bayel.durusbekov.kk@gmail.com>
2022-01-27 14:43:56 +0600

    commit boldu

--- src/screens/MainSreen.js
+++ src/screens/MainSreen.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import { Text, TextInput, View } from "react-native";
 
 const MainSreen = () => {
   return (
     <View style={{flex: 1}}>
-      <Text>
-          Ulukbey
-      </Text>
+      <TextInput>
+
+      </TextInput>
     </View>
   );
 };
