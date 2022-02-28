c35ea7742a4b3e20817ab1a1c25fb598f207c4a0
Bekbolot Tazhibaev <tabekg@gmail.com>
2022-02-28 11:46:11 +0600

    icon android

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,6 +1,11 @@
-export const APP_TITLE = 'Besoft School';
+export const DEBUG_MODE = process.env.NODE_ENV !== 'production';
 
-export const API_URL = 'http://192.168.0.107:5002';
+export const APP_TITLE = 'Besoft School';
+//export const APP_VERSION = 1;
+
+export const API_URL = DEBUG_MODE
+  ? 'http://192.168.0.107:5002'
+  : 'https://api.school.besoft.kg';
 export const API_VERSION = 1;
 
 export const VERIFICATION_CODE_LENGTH = 4;
