915aff871c9bd1a158e2074bf654ad69d0c087ce
Bekbolot Tazhibaev <tabekg@gmail.com>
2022-03-01 12:15:07 +0600

    init

---
+++ src/Root.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper';
+//import {NavigationContainer} from '@react-navigation/native';
+//import App from './App';
+//import {Provider} from 'react-redux';
+//import stores from './stores';
+//import {teal} from 'material-ui-colors';
+
+// const theme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     primary: teal[900],
+//     accent: '#f1c40f',
+//   },
+// };
+
+const Root = () => {
+  return (
+    <Provider store={stores}>
+      <NavigationContainer>
+        <PaperProvider theme={theme}>
+          <StatusBar barStyle={'light-content'} />
+          <App />
+        </PaperProvider>
+      </NavigationContainer>
+    </Provider>
+  );
+};
+
+export default Root;
